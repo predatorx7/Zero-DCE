d0aaa370df063f83cf99c6080dcd9cf6bcc32d80
predatorx7 <smushaheed@outlook.com>
2021-12-26 04:10:14 +0530

    Add rough code for isolate usage and image manipulation

--- flutter_app/lib/src/di/model_interpretor.dart
+++ flutter_app/lib/src/di/model_interpretor.dart
@@ -2,6 +2,7 @@ import 'package:cat/src/model/tflite.dart';
 import 'package:magnific_core/magnific_core.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 class ModelInterpretorProvider extends StateNotifier<tfl.Interpreter?> {
   ModelInterpretorProvider() : super(null);
@@ -15,9 +16,32 @@ class ModelInterpretorProvider extends StateNotifier<tfl.Interpreter?> {
       final interpreter = await downloader.getModelInterpreter(modelName);
       logger.config('Model download completed');
       state = interpreter;
+      Future.microtask(() {
+        _loadModel(interpreter);
+      });
       logger.config('Model\'s interpreter supplied to the provider');
     } catch (e, s) {
       logger.severe('Failed to download model', e, s);
+    }
+  }
+
+  /// Shapes of output tensors
+  List<List<int>>? _outputShapes;
+
+  /// Types of output tensors
+  List<TfLiteType>? _outputTypes;
+
+  void _loadModel(Interpreter interpreter) {
+    try {
+      final outputTensors = interpreter.getOutputTensors();
+      _outputShapes = const [];
+      _outputTypes = const [];
+      for (var tensor in outputTensors) {
+        _outputShapes!.add(tensor.shape);
+        _outputTypes!.add(tensor.type);
+      }
+    } catch (e, s) {
+      logger.severe('Failed to load model', e, s);
     }
   }
 
